f46d9ec581347c8660f7fab75e7ac48c20fff9f6
imanuel22 <imanuelrenra220@gmail.com>
2024-05-24 20:06:23 +0800

    update

--- resources/views/desawisata/create.blade.php
+++ resources/views/desawisata/create.blade.php
@@ -29,7 +29,7 @@
     <div class="mb-5">
         <label for="deskripsi" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">deskripsi Desawisata</label>
         <textarea name="deskripsi" id="deskripsi" rows="3" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">@if ('deskripsi')
-            {{old('deskripsi')}}@endif</textarea>
+        {{old('deskripsi')}}@endif</textarea>
             
         @error('deskripsi')    
         <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">Oops!</span> Username already taken!</p>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Http\Controllers\Desawisata;
+use App\Http\Controllers\DesawisataController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',function(){
    return view('auth.login');
 });
 
-Route::resource('/desawisata',Desawisata::class);
+Route::resource('/desawisata',DesawisataController::class);
